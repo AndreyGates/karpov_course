6ee460e71924e0692d2a4c37cf6fb665ff593892
Андрей Писаревский <pisarevskiy1977@gmail.com>
2023-11-15 09:41:28 +0300

    finished memoization

---
+++ src/MEMOIZATION/decorator.py
@@ -0,0 +1,29 @@
+'''Modules'''
+from typing import Callable
+
+def memoize(func: Callable) -> Callable:
+    """
+    Memoize function
+    """
+    cache = {}
+
+    def wrapper(*args, **kwargs) -> object:
+        """
+        The wrapper collect all the agrument values 
+        and the return value of a function
+        """
+        # gathering positional and keyword agrs
+        # (priorly resolving 'unhashable type: list' issue)
+        key = str(args) + str(kwargs)
+
+        # if the current arguments are new,
+        # calculate the function cache them
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+
+        return cache[key]
+    return wrapper
+
+
+if __name__ == '__main__':
+    print({True: 'yes', 1: 'no', 1.0: 'probably'})
